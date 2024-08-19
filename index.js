031d196df9abc752d2a5ec4fd8ede520d46d3d59
lamlankosi <lamlanomnganga9@gmail.com>
2024-08-19 15:42:22 +0200

    Database Connection

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
-// 
+import express from 'express'
+import path from 'path'
+
+const app = express()
+const port = process.env.PORT || 4000
+
+
+app.use(
+    express.static('./static'),
+    express.json(),
+    express.urlencoded({
+        extended: true
+}))
+
+app.get('^/$|/urbanLoft', (req, res)=>{
+    res.status(200).sendFile(path.resolve('./static/html/index.html'))
+})
+app.listen(port, () => {
+    console.log(`Server is running on ${port}`);
+})
