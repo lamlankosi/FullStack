3dd397bec10f20eb24d3d48f67d1a8fcf35de9c3
lamlankosi <lamlanomnganga9@gmail.com>
2024-08-20 09:06:44 +0200

    Authenticate User

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
 import express from 'express'
 import path from 'path'
+import 'dotenv/config'
 
 const app = express()
-const port = process.env.PORT || 4000
+const port = +process.env.PORT || 4000
 
 
 app.use(
