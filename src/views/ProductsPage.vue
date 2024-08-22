90657b3f7ef3956334c2e25a4211cf212f3d80f8
lamlankosi <lamlanomnganga9@gmail.com>
2024-08-22 12:44:47 +0200

    Products

--- src/views/ProductsPage.vue
+++ src/views/ProductsPage.vue
@@ -1,55 +1,63 @@
 <template>
-    <div>
-      <h1>Our Products</h1>
-      <div v-if="products.length" class="product-list">
-        <div v-for="product in products" :key="product.id" class="product">
-          <router-link :to="`/product/${product.id}`">
-            <img :src="product.image" :alt="product.name">
-            <h3>{{ product.name }}</h3>
-            <p>{{ product.price }}</p>
-          </router-link>
-        </div>
+  <div class="products">
+    <div class="row">
+      <div class="display-2">
+        <h2>Our Products</h2>
       </div>
-      <div v-else>Loading products...</div>
+      {{ this.products }}
+      <div class="row" v-if="products">
+        <Card v-for="product in products" :key="product.prod2ID">
+        <template #cardHeader>
+          <img :src="product.prodUrl" loading="lazy" class="img-fluid" :alt="product.prodName">
+        </template>
+        <template #cardBody>
+          <h5 class="card-title">{{product.prodName}}</h5>
+          <p class="lead">{{ product.Category}}</p>
+          <p class="lead">Amount R{{ product.amount }}</p>
+        </template>
+      </Card>
+      </div>
     </div>
-  </template>
-  
-  <script>
-  import axios from 'axios';
-  
-  export default {
-    data() {
-      return {
-        products: [],
-      };
-    },
-    async created() {
-      try {
-        const response = await axios.get('https://api.example.com/products');
-        this.products = response.data;
-      } catch (error) {
-        console.error('Error fetching products:', error);
+
+  </div>
+</template>
+
+<script>
+
+import Card from '@/components/Card.vue'
+import { computed, onMounted } from 'vue';
+import { useStore } from 'vuex';
+const store = useStore()
+export default {
+  components: {
+    Card
+  },
+  // computed: {
+  //   products(){
+  //     return this.$store.state.products
+  //   }
+  // },
+  // mounted() {
+  //   this.$store?.dispatch('fetchProducts')
+  // },
+  setup(){
+    const products = computed(
+      () => store.state.products
+    )
+    onMounted(
+      () =>{ 
+        store?.dispatch('fetchProducts')
+        console.log(products);
+        
       }
-    },
-  };
-  </script>
-  
-  <style scoped>
-  .product-list {
-    display: flex;
-    flex-wrap: wrap;
+      
+      
+    )
   }
-  .product {
-    width: calc(33.333% - 20px);
-    margin: 10px;
-    padding: 20px;
-    background: #FFF;
-    border: 1px solid #DDD;
-    text-align: center;
-  }
-  .product img {
-    max-width: 100%;
-    height: auto;
-  }
-  </style>
-  
+
+}
+</script>
+
+<style>
+
+</style>
