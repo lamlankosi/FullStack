8c84858325210853012b908e481c8c66fcaafd4b
lamlankosi <lamlanomnganga9@gmail.com>
2024-08-24 13:30:01 +0200

    error message

--- index.js
+++ index.js
@@ -10,11 +10,11 @@ const port = +process.env.PORT || 4000
 
 app.use((req, res, next) => {
     res.header("Access-Control-Allow-Origin", "*");
-    // res.header("Access-Control-Allow-Credentials", "true");
+    res.header("Access-Control-Allow-Credentials", "true");
     res.header("Access-Control-Allow-Methods", "*");
-    // res.header("Access-Control-Request-Methods", "*");
-    // res.header("Access-Control-Allow-Headers", "*");
-    // res.header("Access-Control-Expose-Headers", "Authorization");
+    res.header("Access-Control-Request-Methods", "*");
+    res.header("Access-Control-Allow-Headers", "*");
+    res.header("Access-Control-Expose-Headers", "Authorization");
 
     next()
 })
