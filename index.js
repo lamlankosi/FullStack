2bbf42f98bb429cf447dfdc74fcb245c56e2c7d3
lamlankosi <lamlanomnganga9@gmail.com>
2024-08-24 13:21:04 +0200

    error message

--- index.js
+++ index.js
@@ -6,22 +6,18 @@ import { errorHandling } from './middleware/ErrorHandling.js'
 
 const app = express()
 const port = +process.env.PORT || 4000
-const cors = require('cors');
-app.use(cors())
+
+
 app.use((req, res, next) => {
-    res.header("Access-Control-Allow-Origin", "http://localhost:8080"); // Specific origin
+    res.header("Access-Control-Allow-Origin", "*");
     res.header("Access-Control-Allow-Credentials", "true");
-    res.header("Access-Control-Allow-Methods", "GET, POST, PATCH, DELETE, OPTIONS");
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
+    res.header("Access-Control-Allow-Methods", "*");
+    res.header("Access-Control-Request-Methods", "*");
+    res.header("Access-Control-Allow-Headers", "*");
     res.header("Access-Control-Expose-Headers", "Authorization");
 
-    // Handle preflight requests
-    if (req.method === 'OPTIONS') {
-        return res.status(200).end();
-    }
-
-    next();
-});
+    next()
+})
 app.use(
     express.static('./static'),
     express.json(),
